45e491f4f9ef03efcbd22f9e3c3c022157fc6365
JosephOkumu <jokumu25@gmail.com>
2024-09-30 20:14:17 +0300

    feat: add function for calculating linear regression

---
+++ calcs/regression.go
@@ -0,0 +1,26 @@
+package calcs
+
+// function to calculate linear regression
+func LinearRegression(inputData []float64) (float64, float64) {
+	length := float64(len(inputData))
+
+	var addY float64
+	for _, y := range inputData {
+		addY += y
+	}
+
+	meanX := (length - 1) / 2
+	meanY := Mean(inputData)
+
+	numerator := 0.0
+	denominator := 0.0
+
+	for i := 0; i < int(length); i++ {
+		numerator += (float64(i) - meanX) * (inputData[i] - meanY)
+		denominator += (float64(i) - meanX) * (float64(i) - meanX)
+	}
+
+	m := numerator / denominator
+	b := meanY - m*meanX
+	return m, b
+}
